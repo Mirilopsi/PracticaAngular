9e2711a1ca5c9231d52d4c7f811fc70d709e5e27
Miriam López Sierra <lopezsierramiriam@gmail.com>
2017-05-12 11:42:33 +0200

    .

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -4,8 +4,5 @@ export const BackendUri: OpaqueToken = new OpaqueToken("BackendUri");
 
 export const BackendUriProvider = {
     provide: BackendUri,
-    useValue: {
-        server: "http://localhost:3004",
-        app: "http://localhost:4200"
-    }
+    useValue: "http://localhost:3004"
 };
